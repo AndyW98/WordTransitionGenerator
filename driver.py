b5a8614b82f712c0f1a5fe7dea93be0f735ba95d
andybola <wallace645@gmail.com>
2019-10-17 22:20:58 -0400

    Create training files and read capability

---
+++ driver.py
@@ -0,0 +1,8 @@
+from reader import Reader
+
+def main():
+    reader = Reader()
+    reader.read(True)
+
+if __name__ == "__main__":
+    main()
---
+++ reader.py
@@ -0,0 +1,33 @@
+IGNORE_GRAMMAR = ['!', '.', ',', '?', ';']
+
+class Reader():
+
+    def __init__(self):
+        self.folder = ".\\Training Files\\"
+        self.file = "dracula.txt"
+
+    def read(self, gram_remove=False):
+        filepath = self.folder + self.file
+        with open(filepath, 'r') as f:
+            lines = f.readlines()
+        for i in range(len(lines)):
+            lines[i] = lines[i].rstrip('\n')
+        while('' in lines):
+            lines.remove('')
+        
+        words = []
+        for line in lines:
+            temp = line.split()
+            for word in temp:
+                words.append(word)
+        
+        self.words = words
+        if gram_remove:
+            self.remove_grammar()
+
+    def remove_grammar(self):
+        for grammar in IGNORE_GRAMMAR:
+            for i in range(len(self.words)):
+                self.words[i] = self.words[i].rstrip(grammar)
+        while('' in self.words):
+            self.words.remove('')
